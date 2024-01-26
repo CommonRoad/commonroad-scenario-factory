5cafef467787f773268abb873ca0b60a95f86a4d
Boran Sivrikaya <boran.sivrikaya@tum.de>
2024-01-26 09:15:41 +0100

    Dummy test added, main pipeline scripts renamed

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,4 +1,13 @@
 import pytest
+import importlib
+from files.dummy import add
+
+module_name = 'files/1_bounding_box_coordinates'
+script_module = importlib.import_module(module_name)
+
+script_module.compute_bounding_box_coordinates(1,2,3)
+
+assert add(2, 3) == 6
 
 
 
