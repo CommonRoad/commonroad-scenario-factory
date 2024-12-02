a83369e5471e3fd72a085acd2c4a7ef0609b90c0
Jan-Niklas Weghorn <jrester379@gmail.com>
2024-12-02 13:08:36 +0100

    feat(scenario_generation): reference goal lanelet only if it is different from initial lanelet

--- tests/unit/test_scenario_generation.py
+++ tests/unit/test_scenario_generation.py
@@ -2,7 +2,11 @@ import numpy as np
 from commonroad.scenario.scenario import Scenario
 from commonroad.scenario.state import ExtendedPMState
 
+from scenario_factory.builder import LaneletNetworkBuilder
+from scenario_factory.builder.dynamic_obstacle_builder import DynamicObstacleBuilder
+from scenario_factory.builder.trajectory_builder import TrajectoryBuilder
 from scenario_factory.scenario_generation import (
+    create_planning_problem_for_ego_vehicle,
     create_planning_problem_set_and_solution_for_ego_vehicle,
     delete_colliding_obstacles_from_scenario,
 )
@@ -32,6 +36,54 @@ class TestDeleteCollidingObstaclesFromScenario:
         deleted_obstacles = delete_colliding_obstacles_from_scenario(scenario, all=True)
         assert len(deleted_obstacles) == 4
         assert len(scenario.dynamic_obstacles) == 0
+
+
+class TestCreatePlanningPorlbmeForEgoVehicle:
+    def test_does_not_assign_goal_region_to_lanelet_if_goal_and_initial_state_are_on_same_lanelet(
+        self,
+    ):
+        lanelet_network_builder = LaneletNetworkBuilder()
+        lanelet1 = lanelet_network_builder.add_lanelet((0.0, 0.0), (0.0, 10.0), width=5)
+        lanelet_network = lanelet_network_builder.build()
+        ego_vehicle = (
+            DynamicObstacleBuilder(dynamic_obstacle_id=1)
+            .set_trajectory(
+                TrajectoryBuilder()
+                .start(0, position=lanelet1.center_vertices[0], velocity=25.0, orientation=1.0)
+                .end(10, position=lanelet1.center_vertices[-1], velocity=23.4, orientation=1.0)
+            )
+            .build()
+        )
+        planning_problem = create_planning_problem_for_ego_vehicle(lanelet_network, ego_vehicle)
+        assert planning_problem.planning_problem_id == ego_vehicle.obstacle_id
+        assert len(planning_problem.goal.state_list) == 1
+        assert planning_problem.goal.lanelets_of_goal_position is None
+
+    def test_assigns_goal_region_to_lanelet_if_goal_and_initial_state_are_on_different_lanelets(
+        self,
+    ):
+        lanelet_network_builder = LaneletNetworkBuilder()
+        lanelet1 = lanelet_network_builder.add_lanelet((0.0, 0.0), (0.0, 10.0), width=5)
+        lanelet2 = lanelet_network_builder.add_adjacent_lanelet(lanelet1, width=5)
+        lanelet_network = lanelet_network_builder.build()
+        ego_vehicle = (
+            DynamicObstacleBuilder(dynamic_obstacle_id=1)
+            .set_trajectory(
+                TrajectoryBuilder()
+                .start(0, position=lanelet1.center_vertices[0], velocity=25.0, orientation=1.0)
+                .end(10, position=lanelet2.center_vertices[-1], velocity=23.4, orientation=1.0)
+            )
+            .build()
+        )
+
+        planning_problem = create_planning_problem_for_ego_vehicle(
+            lanelet_network, ego_vehicle, planning_problem_with_lanelet=True
+        )
+        assert planning_problem.planning_problem_id == ego_vehicle.obstacle_id
+        assert len(planning_problem.goal.state_list) == 1
+        assert planning_problem.goal.lanelets_of_goal_position is not None
+        assert len(planning_problem.goal.lanelets_of_goal_position.values()) == 1
+        assert planning_problem.goal.lanelets_of_goal_position[0] == [lanelet2.lanelet_id]
 
 
 class TestCreatePlanningProblemSetAndSolutionForEgoVehicle:
